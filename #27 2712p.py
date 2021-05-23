61af39d2c19fa815803a89fdf8ef3b9500ec3890
gbrs <gbrs@mail.ru>
2021-05-23 15:32:21 +0300

    неправильно было 2712p, получается O(n^3). Что-то не так

--- #27 2712p.py
+++ #27 2712p.py
@@ -4,21 +4,21 @@
 Порядок элементов тройки должен соответствовать порядку в последовательности.
 '''
 
-with open('#27 2982p.txt') as f:
+with open('#27 2712p_small.txt') as f:
     lst = []
     n = int(f.readline())
     for i in range(n):
         lst.append(int(f.readline().strip()))
 
 st = set(lst)
-# print(lst)
+print(lst)
 
 cnt = 0
-for i in range(60000 - 1):
-    for j in range(i + 1, 60000):
-        if lst[i] + lst[j] in st:
-            cnt += 1
-    if i % 100 == 0:
-        print(i)
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        for k in range(i + 2, n):
+            if lst[i] + lst[j] == lst[k]:
+                print(lst[i], lst[j], lst[k])
+                cnt += 1
 
 print(cnt)
