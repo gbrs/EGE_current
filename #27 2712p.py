aa67289a59f6bd5fff2f91ca0c77e906824c8cd4
gbrs <gbrs@mail.ru>
2021-05-23 16:08:51 +0300

    косметика

--- #27 2712p.py
+++ #27 2712p.py
@@ -4,21 +4,23 @@
 Порядок элементов тройки должен соответствовать порядку в последовательности.
 '''
 
-with open('#27 2712p_small.txt') as f:
+'НЕПРАВИЛЬНЫЙ ОТВЕТ!!!'
+
+with open('#27 2712p.txt') as f:
     lst = []
     n = int(f.readline())
     for i in range(n):
         lst.append(int(f.readline().strip()))
 
-st = set(lst)
-print(lst)
+# print(lst)
 
 cnt = 0
 for i in range(n - 2):
     for j in range(i + 1, n - 1):
-        for k in range(i + 2, n):
+        for k in range(j + 1, n):
             if lst[i] + lst[j] == lst[k]:
-                print(lst[i], lst[j], lst[k])
+                # print(i, lst[i], j, lst[j], k, lst[k])
                 cnt += 1
+        print(i, j)
 
 print(cnt)
