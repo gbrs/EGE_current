885ff6350b2f86c62409ddea73ded5eedeedfd51
gbrs <61385511+gbrs@users.noreply.github.com>
2021-05-24 11:08:53 +0300

    решил 2710p

---
+++ 2710p.py
@@ -0,0 +1,28 @@
+'''
+Дана последовательность N целых неотрицательных чисел.
+Необходимо определить количество пар положительных элементов этой последовательности,
+сумма которых четна, при этом между элементами пары есть хотя бы один ноль.
+'''
+
+with open('2710p.txt') as f:
+    lst = []
+    n = int(f.readline())
+    for _ in range(n):
+        lst.append(int(f.readline()))
+
+
+cnt = odd = even = current_odd = current_even = 0
+
+for number in lst:
+    if number == 0:
+        even += current_even
+        odd += current_odd
+        current_odd = current_even = 0
+    elif number % 2 == 0:
+        cnt += even
+        current_even += 1
+    else:
+        cnt += odd
+        current_odd += 1
+
+print(cnt)
