ca96820c92697c51137383dafe11a590bc7f9419
gbrs <61385511+gbrs@users.noreply.github.com>
2021-06-09 10:58:37 +0300

    решил №17 2753p

--- Probe.py
+++ Probe.py
@@ -1,8 +1,3 @@
-for divisor in range(2, int(number ** 0.5) + 1):
-    if number_copy % divisor == 0:
-        divisor_list.append(divisor)
-        number_copy //= divisor
-    if number_copy % divisor == 0:
-        break
-    if len(divisor_list) > 3:
-        break
+n = 133456
+n = str(n)
+print(len(set(n)))
